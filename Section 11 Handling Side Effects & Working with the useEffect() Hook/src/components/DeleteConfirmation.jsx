8eb1cd1e29921a9eb8956c59789e380ff666e1dc
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-04 18:29:36 +0300

    optimize state updates

--- Section 11 Handling Side Effects & Working with the useEffect() Hook/src/components/DeleteConfirmation.jsx
+++ Section 11 Handling Side Effects & Working with the useEffect() Hook/src/components/DeleteConfirmation.jsx
@@ -1,22 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar.jsx";
 
 const TIMER = 3000;
 
 //use useCallback when passing functions as dependencies of useEffect()
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRemainingTime((prevTime) => prevTime - 10);
-    }, 10);
-
-    return () => {
-      clearInterval(interval);
-    };
-
-  }, []);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
@@ -39,7 +27,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
