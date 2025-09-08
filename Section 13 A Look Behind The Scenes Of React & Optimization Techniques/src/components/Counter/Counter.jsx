a5a559671acc9dc8d5e113f29337b17d7f4fc480
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-08 13:33:42 +0300

    Why Keys Matter When Managing State!

--- Section 13 A Look Behind The Scenes Of React & Optimization Techniques/src/components/Counter/Counter.jsx
+++ Section 13 A Look Behind The Scenes Of React & Optimization Techniques/src/components/Counter/Counter.jsx
@@ -5,13 +5,11 @@ import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log('Calculating if is prime number', 2, 'other');
+
   if (number <= 1) {
     return false;
   }
@@ -27,21 +25,36 @@ function isPrime(number) {
   return true;
 }
 
-//Don't overuse memo()!!! ... use it as high as possible in the component tree
 const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  // different than memo(), useMemo() is used not for component functions like memo()
-  // don't overuse useMemo()!!!
-  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
+
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([
+    { value: initialCount, id: Math.random() * 1000 },
+  ]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
 
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{ value: -1, id: Math.random() * 1000 },
+    ...prevCounterChanges,
+    ]);
   }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [{ value: 1, id: Math.random() * 1000 },
+    ...prevCounterChanges,
+    ]);
   }, []);
 
   return (
@@ -54,11 +67,12 @@ const Counter = memo(function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
 });
