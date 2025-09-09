a75c8b0d45720e565ed0d191ab23639a8920b494
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-09 17:52:49 +0300

    Sending HTTP Requests via useEffect

--- Section 15 Sending Http Requests (e.g. Connecting to a Database)/src/components/AvailablePlaces.jsx
+++ Section 15 Sending Http Requests (e.g. Connecting to a Database)/src/components/AvailablePlaces.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlace, setAvailablePlaces] = useState([]);
 
-  fetch('http://localhost:3000/places')
-    .then((response) => {
-      return response.json();
-    })
-    .then((resData) => {
-      setAvailablePlaces(resData.places);
-    });
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
 
   return (
     <Places
