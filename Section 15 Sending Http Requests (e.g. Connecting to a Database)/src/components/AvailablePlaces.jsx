78c46e531b97b82e8387a1e92a529dbf49cd3650
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-09 18:04:24 +0300

    Handling Loading States

--- Section 15 Sending Http Requests (e.g. Connecting to a Database)/src/components/AvailablePlaces.jsx
+++ Section 15 Sending Http Requests (e.g. Connecting to a Database)/src/components/AvailablePlaces.jsx
@@ -3,13 +3,16 @@ import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlace, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -19,6 +22,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlace}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
