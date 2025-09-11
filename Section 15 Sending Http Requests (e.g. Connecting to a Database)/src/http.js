f19b77d1b46aaeace48daf3d187f1807a0a37307
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-11 18:06:40 +0300

    Practice: Fetching Data

--- Section 15 Sending Http Requests (e.g. Connecting to a Database)/src/http.js
+++ Section 15 Sending Http Requests (e.g. Connecting to a Database)/src/http.js
@@ -26,3 +26,14 @@ export async function updateUserPlaces(places) {
 
     return resData.message;
 }
+
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const resData = await response.json();
+
+    if (!response.ok) { // 200, 300...
+        throw new Error('Failed to fetch user places.');
+    }
+
+    return resData.places;
+}
