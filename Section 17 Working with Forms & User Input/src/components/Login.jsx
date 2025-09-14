89e99c289a9be79270785a8629fd8c2a9fa41e47
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-14 13:50:40 +0300

    Managing & Getting User Input via State & Generic Handlers

--- Section 17 Working with Forms & User Input/src/components/Login.jsx
+++ Section 17 Working with Forms & User Input/src/components/Login.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('Submitted');
+
+    console.log(enteredValues);
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value
+    }))
   }
 
   return (
@@ -11,12 +37,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange('password', event.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
