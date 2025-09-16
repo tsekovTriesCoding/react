07d5b52d680891ace71f50b8d372eac5118c74fe
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-16 17:45:44 +0300

    Adding Validation Checks

--- Section 18 Handling Forms via Form Actions/src/components/Signup.jsx
+++ Section 18 Handling Forms via Form Actions/src/components/Signup.jsx
@@ -1,7 +1,45 @@
+import { hasMinLength, isEmail, isEqualToOtherValue, isNotEmpty } from "../util/validation.js";
+
 export default function Signup() {
   function signupAction(formData) {
-    const enteredEmail = formData.get('email');
-    console.log(enteredEmail);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirm-password');
+    const firstName = formData.get('first-name');
+    const lastName = formData.get('last-name');
+    const role = formData.get('role');
+    const terms = formData.get('terms');
+    const acquisitionChannel = formData.getAll('acquisition');
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push('Invalid email address.');
+    }
+
+    if (!isNotEmpty(password) || hasMinLength(password, 6)) {
+      errors.push('You must provide a password with atleast 6 characters.');
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push('Password do not match.');
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push('Please provide both your first and last name.');
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push('Please select a role');
+    }
+
+    if (!terms) {
+      errors.push('You must agree to the terms and conditions.');
+    }
+
+    if (acquisitionChannel.length === 0) {
+      errors.push('Please select atleast one acquisition channel.');
+    }
   }
 
   return (
