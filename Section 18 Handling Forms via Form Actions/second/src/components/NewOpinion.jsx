1b1b6134f1381029901a0fc8f76300f4b6f7677c
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-17 11:46:14 +0300

    Updating the UI with useFormStatus()

--- Section 18 Handling Forms via Form Actions/second/src/components/NewOpinion.jsx
+++ Section 18 Handling Forms via Form Actions/second/src/components/NewOpinion.jsx
@@ -1,5 +1,6 @@
 import { useActionState, use } from "react";
 import { OpinionsContext } from "../store/opinions-context.jsx";
+import Submit from "./Submit.jsx";
 
 export function NewOpinion() {
   const { addOpinion } = use(OpinionsContext);
@@ -66,9 +67,7 @@ export function NewOpinion() {
           ))}
         </ul>}
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        <Submit />
       </form>
     </div>
   );
