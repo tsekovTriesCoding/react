295238c95fca80047519ebdb5cf3720bf0cd3dfe
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-20 16:34:59 +0300

    Migrating To Form Actions

--- Section 19 Practice Project Building a Food Order App/src/components/Checkout.jsx
+++ Section 19 Practice Project Building a Food Order App/src/components/Checkout.jsx
@@ -45,13 +45,10 @@ export default function Checkout() {
         clearData();
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        const fd = new FormData(event.target);
+    async function checkoutAction(fd) {
         const customerData = Object.fromEntries(fd.entries());
 
-        sendRequest(JSON.stringify({
+        await sendRequest(JSON.stringify({
             order: {
                 items: cartCtx.items,
                 customer: customerData
@@ -93,7 +90,7 @@ export default function Checkout() {
             open={userProgressCtx.progress === 'checkout'}
             onClose={handleClose}
         >
-            <form onSubmit={handleSubmit}>
+            <form action={checkoutAction}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
