0981af0eedef66a7e668feb084be244cee15e713
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-20 16:30:40 +0300

    Finishing Touches

--- Section 19 Practice Project Building a Food Order App/src/hooks/useHttp.js
+++ Section 19 Practice Project Building a Food Order App/src/hooks/useHttp.js
@@ -17,11 +17,15 @@ export default function useHttp(url, config, initialData) {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
-    const sendRequest = useCallback(async function sendRequest() {
+    function clearData() {
+        setData(initialData);
+    }
+
+    const sendRequest = useCallback(async function sendRequest(data) {
         setIsLoading(true);
 
         try {
-            const resData = await sendHttpRequest(url, config);
+            const resData = await sendHttpRequest(url, { ...config, body: data });
             setData(resData);
         } catch (error) {
             setError(error.message || 'Something went wrong!');
@@ -41,6 +45,7 @@ export default function useHttp(url, config, initialData) {
         data,
         isLoading,
         error,
-        sendRequest
+        sendRequest,
+        clearData
     };
 }
