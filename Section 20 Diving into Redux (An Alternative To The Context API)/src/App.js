7e7de44e60382c69ddc0eefaacad384fca6b4198
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-22 17:45:59 +0300

    Reading & Dispatching From A New Slice

--- Section 20 Diving into Redux (An Alternative To The Context API)/src/App.js
+++ Section 20 Diving into Redux (An Alternative To The Context API)/src/App.js
@@ -1,14 +1,19 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
 import Counter from './components/Counter.js';
-import Header from './components/Header.js'
-import Auth from './components/Auth.js'
+import Header from './components/Header.js';
+import Auth from './components/Auth.js';
+import UserProfile from './components/UserProfile.js';
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
