956a8067113ea006f67d978248561a40e575b2d7
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-22 16:52:20 +0300

    Migrating Everything To Redux Toolkit

--- Section 20 Diving into Redux (An Alternative To The Context API)/src/store/index.js
+++ Section 20 Diving into Redux (An Alternative To The Context API)/src/store/index.js
@@ -1,9 +1,8 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
@@ -11,7 +10,7 @@ createSlice({
             state.counter++;
         },
         increase(state, action) {
-            state.counter += action.amount;
+            state.counter += action.payload;
         },
         decrement(state) {
             state.counter--;
@@ -22,38 +21,9 @@ createSlice({
     }
 });
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        }
-    }
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        }
-    }
-
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        }
-    }
-
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter,
-        }
-    }
-
-    return state;
-}
-
-const store = createStore(counterReducer);
-
+export const counterActions = counterSlice.actions;
 export default store;
