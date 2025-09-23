9e0ec732f857487b24c59a3f6198ae29a67632f9
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-23 16:37:39 +0300

    Using useEffect with Redux

--- Section 21 Advanced Redux/src/App.js
+++ Section 21 Advanced Redux/src/App.js
@@ -1,10 +1,22 @@
 import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+
+import Cart from './components/Cart/Cart.js';
+import Layout from './components/Layout/Layout.js';
+import Products from './components/Shop/Products.js';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart); // to listen for changes in cart state
+
+  useEffect(() => {
+    fetch('https://react-http-39c.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    }).catch((error) => {
+      console.error('Sending cart data failed.', error);
+    });
+  }, [cart]); // run this effect whenever cart state changes
 
   return (
     <Layout>
