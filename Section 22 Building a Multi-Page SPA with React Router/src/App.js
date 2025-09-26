ca8d304c4fa681ab4a8aa9a57de5f55692b9eeff
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-26 15:15:59 +0300

    Exploring an Alternative Way of Defining Routes

--- Section 22 Building a Multi-Page SPA with React Router/src/App.js
+++ Section 22 Building a Multi-Page SPA with React Router/src/App.js
@@ -1,12 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home.js';
 import ProductsPage from './pages/Products.js';
 
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <>
+    <Route path='/' element={<HomePage />} />
+    <Route path='/products' element={<ProductsPage />} />
+  </>
+);
+
+// const router = createBrowserRouter([
+//   { path: '/', element: <HomePage /> },
+//   { path: '/products', element: <ProductsPage /> },
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
