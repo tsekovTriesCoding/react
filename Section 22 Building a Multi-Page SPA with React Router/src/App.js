0139b9cef74d61b6714cdf3b64179cd16259341c
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-27 13:47:11 +0300

    Layouts & Nested Routes

--- Section 22 Building a Multi-Page SPA with React Router/src/App.js
+++ Section 22 Building a Multi-Page SPA with React Router/src/App.js
@@ -1,21 +1,28 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home.js';
 import ProductsPage from './pages/Products.js';
+import RootLayout from './pages/Root.js';
 
-const routeDefinitions = createRoutesFromElements(
-  <>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/products' element={<ProductsPage />} />
-  </>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </>
+// );
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage /> },
-//   { path: '/products', element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ]
+  },
+]);
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
