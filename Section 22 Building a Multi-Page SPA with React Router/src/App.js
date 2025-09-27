3007f02f0bd5efba1a9d43e3f652086230a7460c
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-27 13:53:04 +0300

    Showing Error Pages with errorElement

--- Section 22 Building a Multi-Page SPA with React Router/src/App.js
+++ Section 22 Building a Multi-Page SPA with React Router/src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home.js';
 import ProductsPage from './pages/Products.js';
 import RootLayout from './pages/Root.js';
+import ErrorPage from './pages/Error.js';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <>
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
