c9d911afcd5681fe8da7361e1100609e91cb2582
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-28 19:03:20 +0300

    Extracting Error Data & Throwing Responses

--- Section 22 Building a Multi-Page SPA with React Router/second/frontend/src/pages/Error.js
+++ Section 22 Building a Multi-Page SPA with React Router/second/frontend/src/pages/Error.js
@@ -1,5 +1,31 @@
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent.js";
+import MainNavigation from "../components/MainNavigation.js";
+
 function ErrorPage() {
-    return <h1>An error occurred!</h1>;
+    const error = useRouteError();
+
+    let title = 'An error occurred!';
+    let message = 'Sorry, an unexpected error has occurred.';
+
+    if (error.status === 500) {
+        message = JSON.parse(error.data).message;
+    }
+
+    if (error.status === 404) {
+        title = 'Not found!';
+        message = 'Could not find resource or page.';
+    }
+
+    return (
+        <>
+            <MainNavigation />
+            <PageContent title={title}>
+                <p>{message}</p>
+            </PageContent>
+        </>
+    );
 }
 
 export default ErrorPage;
