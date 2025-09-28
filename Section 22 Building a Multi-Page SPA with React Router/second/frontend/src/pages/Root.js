240f6007531514d381c41f03a27fa10eb5560691
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-28 18:43:43 +0300

    Reflecting The Current Navigation State in the UI

--- Section 22 Building a Multi-Page SPA with React Router/second/frontend/src/pages/Root.js
+++ Section 22 Building a Multi-Page SPA with React Router/second/frontend/src/pages/Root.js
@@ -3,10 +3,13 @@ import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation.js";
 
 function RootLayout() {
+    // const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
                 <Outlet />
             </main>
         </>
