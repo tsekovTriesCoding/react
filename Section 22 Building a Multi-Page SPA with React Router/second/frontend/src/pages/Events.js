d72ab8463599f47131328d35dd0b2f90b0bc6cc6
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-09-28 14:23:21 +0300

    Data Fetching with a loader()

--- Section 22 Building a Multi-Page SPA with React Router/second/frontend/src/pages/Events.js
+++ Section 22 Building a Multi-Page SPA with React Router/second/frontend/src/pages/Events.js
@@ -1,24 +1,37 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
-const EVENTS = [
-    { id: 'e1', title: 'event 1' },
-    { id: 'e2', title: 'event 2' },
-    { id: 'e3', title: 'event 3' },
-];
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
-    return (
-        <>
-            <h1>The Events Page</h1>
-            < ul>
-                {EVENTS.map((event) => (
-                    <li key={event.id}>
-                        <Link to={event.id}>{event.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        setError('Fetching events failed.');
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
+  );
 }
 
 export default EventsPage;
