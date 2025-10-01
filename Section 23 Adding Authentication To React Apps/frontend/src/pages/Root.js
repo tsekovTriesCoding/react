ada389d858f2b202d2e73874cd1f688f6593449d
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-01 15:24:58 +0300

    Managing the Token Expiration

--- Section 23 Adding Authentication To React Apps/frontend/src/pages/Root.js
+++ Section 23 Adding Authentication To React Apps/frontend/src/pages/Root.js
@@ -1,9 +1,34 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    const timer = setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, tokenDuration);
+
+    return () => {
+      clearTimeout(timer);
+    };
+    
+  }, [token, submit]);
 
   return (
     <>
--- Section 23 Adding Authentication To React Apps/frontend/src/util/auth.js
+++ Section 23 Adding Authentication To React Apps/frontend/src/util/auth.js
@@ -1,21 +1,40 @@
-import { redirect } from "react-router-dom";
+import { redirect } from 'react-router-dom';
+
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem('expiration');
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+}
 
 export function getAuthToken() {
-    const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
-    return token;
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return 'EXPIRED';
+  }
+
+  return token;
 }
 
 export function tokenLoader() {
-    return getAuthToken();
+  const token = getAuthToken();
+  return token;
 }
 
 export function checkAuthLoader() {
-    const token = getAuthToken();
+  const token = getAuthToken();
 
-    if (!token) {
-        return redirect('/auth');
-    }
+  if (!token) {
+    return redirect('/auth');
+  }
 
-    return null;
+  return null;
 }
