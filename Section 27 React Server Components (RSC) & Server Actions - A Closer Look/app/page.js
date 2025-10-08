d236ec8f6935a81c2ab98aedafaf66cf0e15f1f2
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-08 10:41:00 +0300

    Submitting Data with Server Actions

--- Section 27 React Server Components (RSC) & Server Actions - A Closer Look/app/page.js
+++ Section 27 React Server Components (RSC) & Server Actions - A Closer Look/app/page.js
@@ -1,9 +1,9 @@
-import DataFetchingDemo from "@/components/DataFetchingDemo";
+import ServerActionsDemo from "@/components/ServerActionsDemo";
 
 export default function Home() {
   return (
     <main>
-      <DataFetchingDemo />
+      <ServerActionsDemo />
     </main>
   );
 }
