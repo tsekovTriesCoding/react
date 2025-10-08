fde81ec8578caaaef8ee2d69c529c56aab18769e
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-08 11:19:00 +0300

    Handling Errors

--- Section 27 React Server Components (RSC) & Server Actions - A Closer Look/app/page.js
+++ Section 27 React Server Components (RSC) & Server Actions - A Closer Look/app/page.js
@@ -1,21 +1,28 @@
 import fs from 'node:fs/promises';
 
-import UsePromiseDemo from "@/components/UsePromisesDemo";
+import UsePromiseDemo from "@/components/UsePromisesDemo.js";
 import { Suspense } from "react";
+import ErrorBoundary from '@/components/ErrorBoundary.js';
+
+
 
 export default async function Home() {
-  const fetchUsersPromise = new Promise((resolve) =>
+  const fetchUsersPromise = new Promise((resolve, reject) =>
     setTimeout(async () => {
       const data = await fs.readFile('dummy-db.json', 'utf-8');
       const users = JSON.parse(data);
-      resolve(users);
+      // resolve(users);
+
+      reject(new Error('Error!'));
     }, 2000));
 
   return (
     <main>
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo usersPromise={fetchUsersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
