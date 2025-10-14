e3a1a5de9916d04483ffeb3e65a618bd59530dfc
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-14 20:34:35 +0300

    Adding Styling

--- Section 32 React + TypeScript/more-typescript/src/components/TodoItem.tsx
+++ Section 32 React + TypeScript/more-typescript/src/components/TodoItem.tsx
@@ -1,5 +1,14 @@
+import classes from "./TodoItem.module.css";
+
 const TodoItem: React.FC<{ item: { id: string, text: string } }> = (props) => {
-    return <li key={props.item.id}>{props.item.text}</li>
+    return (
+        <li
+            className={classes.item}
+            key={props.item.id}
+        >
+            {props.item.text}
+        </li>
+    );
 }
 
 export default TodoItem;
