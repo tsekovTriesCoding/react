f926bc00573c36f674fdbe0d6446c6722819c0c1
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-14 10:59:42 +0300

    Working with Array & Object Types

--- Section 32 React + TypeScript/test-typescript/basics.ts
+++ Section 32 React + TypeScript/test-typescript/basics.ts
@@ -4,4 +4,25 @@ let age: number = 2;
 
 let username: string = 'John';
 
-let isInstructor : boolean = true;
+let isInstructor: boolean = true;
+
+//More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sprots', 'Cooking'];
+
+let person: {
+    name: string,
+    age: number
+};
+
+person = {
+    name: "Max",
+    age: 32
+}
+
+let people: {
+    name: string,
+    age: number
+}[];
