d3fc7759ad781eb27fa28ccd5e52e94bec73922f
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-17 11:31:49 +0300

    Preparing the App For State Management

--- Section 33 React Summary & Core Feature Walkthrough/src/components/PostsList.jsx
+++ Section 33 React Summary & Core Feature Walkthrough/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 
 function PostsList() {
     return (
-        <ul className={classes.posts}>
-            <Post author="Anton" body="React.js is awesome!" />
-            <Post author="Manuel" body="Checkout the full course!" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Anton" body="React.js is awesome!" />
+                <Post author="Manuel" body="Checkout the full course!" />
+            </ul>
+        </>
     );
 }
 
