acdcdc551d428b1bdd49a4ff4f3a07b171e1e407
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-17 11:28:30 +0300

    Exercise & Another Component

--- Section 33 React Summary & Core Feature Walkthrough/src/App.jsx
+++ Section 33 React Summary & Core Feature Walkthrough/src/App.jsx
@@ -1,10 +1,9 @@
-import Post from "./components/Post";
+import PostsList from "./components/PostsList";
 
 function App() {
   return (
     <main>
-      <Post author="Anton" body="Reeact.js is awesome!" />
-      <Post author="Manuel" body="Checkout the full course!" />
+      <PostsList />
     </main>
   );
 }
