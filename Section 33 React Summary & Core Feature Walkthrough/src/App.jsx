6507ce9ead40770927f3b46b4b2717d6f93cda3a
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-17 14:08:54 +0300

    Handling Form Submission

--- Section 33 React Summary & Core Feature Walkthrough/src/App.jsx
+++ Section 33 React Summary & Core Feature Walkthrough/src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
+import MainHeader from "./components/MainHeader";
 import PostsList from "./components/PostsList";
 
 function App() {
+  const [modalIsVisble, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisble}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 
--- Section 33 React Summary & Core Feature Walkthrough/src/components/PostsList.jsx
+++ Section 33 React Summary & Core Feature Walkthrough/src/components/PostsList.jsx
@@ -1,49 +1,19 @@
-import { useState } from "react";
-
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
-    // let modalContent;
-
-    // if (modalIsVisble) {
-    //     modalContent = (
-    //         <Modal onClose={hideModalHandler}>
-    //             <NewPost
-    //                 onBodyChange={bodyChangeHandler}
-    //                 onAuthorChange={authorChangeHandler}
-    //             />
-    //         </Modal>
-    //     );
-    // }
-
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                     <NewPost
-                        onBodyChange={bodyChangeHandler}
-                        onAuthorChange={authorChangeHandler}
                         onCancel={onStopPosting}
                     />
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
-                <Post author={enteredAuthor} body={enteredBody} />
             </ul>
         </>
     );
