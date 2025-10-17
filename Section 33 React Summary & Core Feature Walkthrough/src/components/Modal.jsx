f608bd288ebcd8bd236f8d1e3a33d67056310843
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-17 12:08:02 +0300

    State & Conditional Content

--- Section 33 React Summary & Core Feature Walkthrough/src/components/Modal.jsx
+++ Section 33 React Summary & Core Feature Walkthrough/src/components/Modal.jsx
@@ -1,9 +1,12 @@
 import classes from './Modal.module.css';
 
-function Modal({ children }) {
+function Modal({ children, onClose }) {
     return (
         <>
-            <div className={classes.backdrop}></div>
+            <div
+                className={classes.backdrop}
+                onClick={onClose}
+            />
             <dialog
                 open
                 className={classes.modal}>
